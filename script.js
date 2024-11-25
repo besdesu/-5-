0bd0e5bd235961f07e1dd0b0a3ad5570e87417d1
besdesu <bhubes.a@ku.th>
2024-11-25 23:04:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const fileInput = document.getElementById('fileInput');
-const gallery = document.getElementById('gallery');
+const gallery = document.querySelector('.gallery');
 
 fileInput.addEventListener('change', (event) => {
     const files = event.target.files;
-    gallery.innerHTML = ''; // Clear gallery content before rendering
+    gallery.innerHTML = ''; // Clear existing images
 
     Array.from(files).forEach(file => {
         const reader = new FileReader();
